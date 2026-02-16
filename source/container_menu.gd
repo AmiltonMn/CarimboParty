0f6718c6303316135fbe314613087a7fdbb378e5
AmiltonMn <amilton.moreiran@hotmail.com>
2026-02-16 01:34:45 -0300

    Pastas padrão do projeto

---
+++ source/container_menu.gd
@@ -0,0 +1,10 @@
+extends VBoxContainer
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_sair_pressed() -> void:
+	pass # Replace with function body.
