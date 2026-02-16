3361a1f19a8ddbab1c51e5746b26cdca3ad285f3
Fernandohka <fernando2005andrade@gmail.com>
2026-02-16 12:26:10 -0300

    inicio guerra

--- source/container_menu.gd
+++ source/container_menu.gd
@@ -1,10 +1,21 @@
 extends VBoxContainer
+
+var mapa_territorios: PackedScene = preload("res://source/Minigames/guerra_de_territorios/mapa.tscn")
+
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_sair_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_jogar_pressed() -> void:
+	get_tree().change_scene_to_packed(mapa_territorios)
+
+
+func _on_conquistas_pressed() -> void:
 	pass # Replace with function body.
