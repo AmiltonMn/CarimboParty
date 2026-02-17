714d75944c662045c89335626f65819da0a0e644
Fernandohka <fernando2005andrade@gmail.com>
2026-02-16 21:09:06 -0300

    é o mapas

---
+++ source/Minigames/guerra_de_territorios/mapa.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var num_players: int = 1
+var carimbo_cena: PackedScene = preload("res://source/Player/carimbo.tscn")
+
+func _ready() -> void:
+	for i in range(num_players):
+		var carimbo = carimbo_cena.instantiate()
+		$"chão/carimbos".add_child(carimbo)
+
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_game_timer_timeout() -> void:
+	pass
+	
--- source/Player/carimbo.gd
+++ source/Player/carimbo.gd
@@ -1,19 +1,32 @@
 extends CharacterBody2D
 
+class_name carimbo
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var can_move: bool = true
 
-signal paint(pos)
+func _ready() -> void:
+	position = Vector2(64, 64)
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
-	move_and_slide()
+	var direction := Input.get_vector("Esquerda", "Direita", "Cima", "Baixo").round()
 	
-	paint.emit(global_position)
+	if direction and can_move:
+		position.x += direction.x * 128
+		position.y += direction.y * 128
+		can_move = false
+		$move_cooldown.start()
+		
+		
+	#if direction:
+		#velocity = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.y = move_toward(velocity.y, 0, SPEED)
+#
+	#move_and_slide()
+
+
+func _on_move_cooldown_timeout() -> void:
+	can_move = true
